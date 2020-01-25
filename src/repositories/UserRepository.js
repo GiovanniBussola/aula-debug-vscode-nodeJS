00458e5468fc1ecb8b9b9d230030d6e1b99149ab
Giovanni Bússola <giovanni.sbussola@gmail.com>
2020-01-25 01:58:33 -0300

    First commit

---
+++ src/repositories/UserRepository.js
@@ -0,0 +1,10 @@
+const users = ['Giovanni', 'Antonio', 'Alcides', 'Silva'];
+
+export function getUsers() {
+    // return users;
+    return []
+}
+
+export function addUser(user) {
+    users.push(user)
+}
